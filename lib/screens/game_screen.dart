59630d20c47af4e8d7648d94c16f5cfadf676247
loloof64 <laurent.bernabe@gmail.com>
2023-02-15 12:11:30 +0100

    started to implement WebRTC signaling

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -15,8 +15,16 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+// Using code from https://github.com/flutter-webrtc/flutter-webrtc-demo/blob/master/lib/src/call_sample/random_string.dart
 
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:logger/logger.dart';
+import 'package:peer_chess/screens/websocket.dart';
 import 'package:simple_chess_board/models/board_arrow.dart';
 import 'package:simple_chess_board/simple_chess_board.dart';
 import 'package:chess_vectors_flutter/chess_vectors_flutter.dart';
@@ -41,6 +49,10 @@ class _GameScreenState extends State<GameScreen> {
   BoardArrow? _lastMoveToHighlight;
   late GameManager _gameManager;
   late HistoryManager _historyManager;
+  late String _turnUserName;
+  late String _turnPassword;
+  late Signaling _signaling;
+  bool _showConnectButton = false;
 
   final ScrollController _historyScrollController =
       ScrollController(initialScrollOffset: 0.0, keepScrollOffset: true);
@@ -55,6 +67,21 @@ class _GameScreenState extends State<GameScreen> {
       onSelectStartPosition: _selectStartPosition,
       isStartMoveNumber: _isStartMoveNumber,
     );
+    _setupTurnCredentials().then((value) {
+      setState(() {
+        _signaling =
+            Signaling(turnUsername: _turnUserName, turnPassword: _turnPassword);
+        _showConnectButton = true;
+      });
+    });
+  }
+
+  Future<void> _setupTurnCredentials() async {
+    final String credentialsString =
+        await rootBundle.loadString('assets/credentials/turn_credentials.json');
+    final data = await json.decode(credentialsString);
+    _turnUserName = data['username'];
+    _turnPassword = data['password'];
   }
 
   bool _isStartMoveNumber(int moveNumber) {
@@ -395,6 +422,53 @@ class _GameScreenState extends State<GameScreen> {
         });
   }
 
+  Future<void> _copyIdToClipboard() async {
+    await Clipboard.setData(
+      ClipboardData(text: _signaling._selfId),
+    );
+  }
+
+  Future<void> _startConnection(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: I18nText('session.dialog_new.title'),
+            content: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                I18nText('session.dialog_new.message'),
+                Container(
+                  color: Colors.grey[300],
+                  child: Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: Text(
+                      _signaling._selfId,
+                      style: const TextStyle(
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: _copyIdToClipboard,
+                  icon: const Icon(
+                    Icons.copy,
+                  ),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: I18nText('buttons.cancel'),
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     final isLandscape =
@@ -404,6 +478,13 @@ class _GameScreenState extends State<GameScreen> {
       appBar: AppBar(
         title: I18nText('game_page.title'),
         actions: [
+          if (_showConnectButton)
+            IconButton(
+              onPressed: () => _startConnection(context),
+              icon: const Icon(
+                Icons.call,
+              ),
+            ),
           IconButton(
             onPressed: _purposeRestartGame,
             icon: const Icon(
@@ -494,5 +575,228 @@ class _GameScreenState extends State<GameScreen> {
               ),
       ),
     );
+  }
+}
+
+class Session {
+  Session({required this.pid});
+  String pid;
+  RTCPeerConnection? pc;
+  RTCDataChannel? dc;
+  List<RTCIceCandidate> remoteCandidates = [];
+}
+
+const asciiStart = 33;
+const asciiEnd = 126;
+
+/// Generates a random integer where [from] <= [to].
+int randomBetween(int from, int to) {
+  if (from > to) throw Exception('$from cannot be > $to');
+  var rand = Random();
+  return ((to - from) * rand.nextDouble()).toInt() + from;
+}
+
+/// Generates a random string of [length] with characters
+/// between ascii [from] to [to].
+/// Defaults to characters of ascii '!' to '~'.
+String randomString(int length, {int from = asciiStart, int to = asciiEnd}) {
+  return String.fromCharCodes(
+      List.generate(length, (index) => randomBetween(from, to)));
+}
+
+enum SignalingState {
+  connectionOpen,
+  connectionClosed,
+  connectionError,
+}
+
+enum CallState {
+  callStateNew,
+  callStateRinging,
+  callStateInvite,
+  callStateConnected,
+  callStateBye,
+}
+
+class Signaling {
+  final JsonEncoder _encoder;
+  final JsonDecoder _decoder;
+
+  String get sdpSemantics => 'unified-plan';
+  SimpleWebSocket? _socket;
+
+  late Session _session;
+
+  final String turnUsername;
+  final String turnPassword;
+  final String _selfId = randomString(15);
+
+  final Map<String, dynamic> _iceServers;
+
+  final Map<String, dynamic> _config = {
+    'mandatory': {},
+    'optional': [
+      {'DtlsSrtpKeyAgreement': true},
+      /*{'RtpDataChannels': true}*/
+    ]
+  };
+
+  final Map<String, dynamic> _dcConstraints = {
+    'mandatory': {
+      'OfferToReceiveAudio': false,
+      'OfferToReceiveVideo': false,
+    },
+    'optional': [],
+  };
+
+  Signaling({
+    required this.turnUsername,
+    required this.turnPassword,
+  })  : _iceServers = {
+          'iceServers': [
+            {
+              'urls': "stun:openrelay.metered.ca:80",
+            },
+            {
+              'urls': "turn:openrelay.metered.ca:80",
+              'username': turnUsername,
+              'credential': turnPassword,
+            },
+            {
+              'urls': "turn:openrelay.metered.ca:443",
+              'username': turnUsername,
+              'credential': turnPassword,
+            },
+            {
+              'urls': "turn:openrelay.metered.ca:443?transport=tcp",
+              'username': turnUsername,
+              'credential': turnPassword,
+            },
+          ]
+        },
+        _encoder = JsonEncoder(),
+        _decoder = JsonDecoder();
+
+  Function(SignalingState state)? onSignalingStateChange;
+  Function(Session session, CallState state)? onCallStateChange;
+
+  Function(Session session, RTCDataChannel dc, RTCDataChannelMessage data)?
+      onDataChannelMessage;
+  Function(Session session, RTCDataChannel dc)? onDataChannel;
+
+  void invite(String peerId) async {
+    Session session = await _createSession(null, peerId: peerId);
+    _session = session;
+    _createDataChannel(session);
+    _createOffer(session);
+    onCallStateChange?.call(session, CallState.callStateNew);
+    onCallStateChange?.call(session, CallState.callStateInvite);
+  }
+
+  Future<Session> _createSession(
+    Session? session, {
+    required String peerId,
+  }) async {
+    var newSession = session ?? Session(pid: peerId);
+    RTCPeerConnection pc = await createPeerConnection({
+      ..._iceServers,
+      ...{'sdpSemantics': sdpSemantics}
+    }, _config);
+    pc.onIceCandidate = (candidate) async {
+      // This delay is needed to allow enough time to try an ICE candidate
+      // before skipping to the next one. 1 second is just an heuristic value
+      // and should be thoroughly tested in your own environment.
+      await Future.delayed(
+          const Duration(seconds: 1),
+          () => _send('candidate', {
+                'to': peerId,
+                'from': _selfId,
+                'candidate': {
+                  'sdpMLineIndex': candidate.sdpMLineIndex,
+                  'sdpMid': candidate.sdpMid,
+                  'candidate': candidate.candidate,
+                },
+              }));
+    };
+
+    pc.onIceConnectionState = (state) {};
+
+    pc.onDataChannel = (channel) {
+      _addDataChannel(newSession, channel);
+    };
+
+    newSession.pc = pc;
+    return newSession;
+  }
+
+  Future<void> _closeSession(Session session) async {
+    await session.pc?.close();
+    await session.dc?.close();
+  }
+
+  void _addDataChannel(Session session, RTCDataChannel channel) {
+    channel.onDataChannelState = (e) {};
+    channel.onMessage = (RTCDataChannelMessage data) {
+      onDataChannelMessage?.call(session, channel, data);
+    };
+    session.dc = channel;
+    onDataChannel?.call(session, channel);
+  }
+
+  Future<void> _createDataChannel(Session session,
+      {label = 'fileTransfer'}) async {
+    RTCDataChannelInit dataChannelDict = RTCDataChannelInit()
+      ..maxRetransmits = 30;
+    RTCDataChannel channel =
+        await session.pc!.createDataChannel(label, dataChannelDict);
+    _addDataChannel(session, channel);
+  }
+
+  Future<void> _createOffer(Session session) async {
+    try {
+      RTCSessionDescription s = await session.pc!.createOffer(_dcConstraints);
+      await session.pc!.setLocalDescription(_fixSdp(s));
+      _send('offer', {
+        'to': session.pid,
+        'from': _selfId,
+        'description': {'sdp': s.sdp, 'type': s.type},
+      });
+    } catch (e) {
+      Logger().e(e.toString());
+    }
+  }
+
+  RTCSessionDescription _fixSdp(RTCSessionDescription s) {
+    var sdp = s.sdp;
+    s.sdp =
+        sdp!.replaceAll('profile-level-id=640c1f', 'profile-level-id=42e032');
+    return s;
+  }
+
+  Future<void> _createAnswer(Session session, String media) async {
+    try {
+      RTCSessionDescription s =
+          await session.pc!.createAnswer(media == 'data' ? _dcConstraints : {});
+      await session.pc!.setLocalDescription(_fixSdp(s));
+      _send('answer', {
+        'to': session.pid,
+        'from': _selfId,
+        'description': {'sdp': s.sdp, 'type': s.type},
+      });
+    } catch (e) {
+      Logger().e(e.toString());
+    }
+  }
+
+  _send(event, data) {
+    var request = {};
+    request["type"] = event;
+    request["data"] = data;
+    _socket?.send(_encoder.convert(request));
+  }
+
+  Future<void> _cleanSession() async {
+    await _session.pc?.close();
+    await _session.dc?.close();
   }
 }
